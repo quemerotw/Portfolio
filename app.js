d1e809b43db8dbd62aa695f8f964533e67b106ad
quemerotw <quemerotw@localhost.localdomain>
2024-09-25 10:17:42 -0300

    black white theme

--- app.js
+++ app.js
@@ -13,3 +13,23 @@ function opentab(nombreMenu) {
     event.currentTarget.classList.add("activeL");
     document.getElementById(nombreMenu).classList.add("activeC");
 }
+
+
+let themes = [];
+let currentThemeIndex = 0;
+
+const boton = document.getElementById("boton-style");
+boton.addEventListener("click", () => {
+  // Remove all themes
+  themes.forEach(theme => document.documentElement.classList.remove(theme));
+
+  // Move to the next theme
+  currentThemeIndex = (currentThemeIndex + 1) % themes.length;
+
+  // Add the new theme
+  document.documentElement.classList.add(themes[currentThemeIndex]);
+});
+
+function loaded(){
+    console.log("te amo mucho mi vida ya vamos");
+}
