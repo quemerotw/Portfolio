509d7f7cf8b89547b841b96c30ab56e629097741
quemerotw <quemerotw@localhost.localdomain>
2024-09-28 06:11:27 -0300

    animacion colores invertidos

--- app.js
+++ app.js
@@ -20,18 +20,42 @@ let currentThemeIndex = 0;
 
 const boton = document.getElementById("boton-style");
 boton.addEventListener("click", () => {
-  // Remove all themes
+
   themes.forEach(theme => document.documentElement.classList.remove(theme));
 
-  // Move to the next theme
   currentThemeIndex = (currentThemeIndex + 1) % themes.length;
 
-  // Add the new theme
   document.documentElement.classList.add(themes[currentThemeIndex]);
 });
 
+
+
+
 function loaded(){
-    console.log("te amo mucho mi vida ya vamos");
+    var box = document.createElement('div');
+    box.className = 'box';
+    box.style.width = '4000px';
+    box.style.height = '4000px';
+    box.style.zIndex = '1000';
+    box.style.background = 'white';
+    box.style.position = 'fixed';
+    box.style.mixBlendMode = 'difference'; // Set mix-blend-mode to multiply
+    box.style.top = '-1500px';
+    box.style.left = '60%';
+    box.style.transformOrigin = 'left';
+    box.style.boxShadow = '10px 10px 50px 50px';
+    document.body.appendChild(box);
+    box.style.transform = 'translateX(-50%) rotate(360deg)'; // Rotate the box 360 degrees
+    setTimeout(function(){
+
+        document.body.removeChild(box);
+
+    }, 5500);
+
+    console.log("ex");
+
+    console.log("ex");
+
 }
 
 var sideMenu = document.getElementById("side-menu");
